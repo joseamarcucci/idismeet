128e1e822cfd55743729b20954106063e9512469
joseamarcucci <74363457+joseamarcucci@users.noreply.github.com>
2021-05-21 11:32:09 -0300

    Update todasu.py

--- todasu.py
+++ todasu.py
@@ -85,24 +85,7 @@ def app():
 
     #st.table(df[['Fecha','Código de reunión','Identificador del participante','Tipo de cliente','Correo electrónico del organizador','Duración','Nombre del participante']])
     asistencia2.index = [""] * len(asistencia2)  
-    report_text = st.text_input("Report Text")
 
-
-    export_as_pdf = st.button("Export Report")
-
-    def create_download_link(val, filename):
-      b64 = pybase64.b64encode(val)  # val looks like b'...'
-      return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
-
-    if export_as_pdf:
-      pdf = FPDF()
-      pdf.add_page()
-      pdf.set_font('Arial', 'B', 16)
-      pdf.cell(40, 10, report_text)
-    
-      html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
-
-      st.markdown(html, unsafe_allow_html=True)
     col.table(asistencia2)
 
 
