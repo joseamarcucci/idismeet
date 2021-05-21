4aedec15d96717c0bdfe3d23a82dc90ea54bb961
joseamarcucci <74363457+joseamarcucci@users.noreply.github.com>
2021-05-21 10:57:25 -0300

    Update gral.py

--- gral.py
+++ gral.py
@@ -5,7 +5,7 @@ def app():
     #st.markdown('<style>div[data-baseweb="select"] > div {text-transform: capitalize;}body{background-color:#008357;}</style>', unsafe_allow_html=True)
 
     st.title('Asistencia ')
-    st.markdown("<p>Seleccionando el mes deseada en el panel izquierdo, se pueden ver los datos totales de:</p><ul><li>Minutos de uso de Collaborate por mes<ul><li>Por Unidad Académica</li><li>Por Salas nativas</li><li>Por Campus </li><li>Cantidad de usuarios</li><li>Grabaciones</li><li>Almacenaminto por UA </li><li>Alamacenamiento total<br /><br /></li></ul></li><li>Usuarios del Campus Learn<ul><li>Por Unidad Académica</li><li>Totales</li><li>Uso General</li></ul></li></ul>", unsafe_allow_html=True)
+    st.markdown("<p>Seleccionando el tipo de información deseada en el panel izquierdo, se pueden ver los datos de:</p><ul><li>Asitencia por docente, fecha y alumnos</li><li>Asistencia por fecha, docente y alumnos</li></ul>", unsafe_allow_html=True)
    
 
    
